0288427245aef7f12bcd4caef6894f65c36287b3
nunezangelr0311-eng <nunezangelr0311@gmail.com>
2025-11-16 16:40:48 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
+
 app.use(express.json());
 
-// Rutas reales del motor
-require("./routes/analyze-nbme")(app);
-require("./routes/generate-plan")(app);
-require("./routes/save-state")(app);
-require("./routes/register-student")(app);
-require("./routes/get-progress")(app);
+// Rutas reales del motor (SIN /routes/)
+require("./analyze-nbme")(app);
+require("./generate-plan")(app);
+require("./save-state")(app);
+require("./register-student")(app);
+require("./get-progress")(app);
 
 // Healthcheck
 app.get("/", (req, res) => {
